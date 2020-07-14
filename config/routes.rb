97995a6dbc141733697b0b537aa974f997757ac9
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-07-14 09:27:14 +0900

    Revert "o"
    This reverts commit 8996a43bad83e2b06e83bf40be8eb406bbfdfd2b.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get 'rails/g'
-  get 'rails/controller'
-  get 'rails/cards'
-  get 'rails/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "cards#index"
 end
