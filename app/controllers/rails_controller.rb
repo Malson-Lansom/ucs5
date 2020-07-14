8996a43bad83e2b06e83bf40be8eb406bbfdfd2b
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-07-14 09:27:11 +0900

    o

---
+++ app/controllers/rails_controller.rb
@@ -0,0 +1,13 @@
+class RailsController < ApplicationController
+  def g
+  end
+
+  def controller
+  end
+
+  def cards
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'rails/g'
+  get 'rails/controller'
+  get 'rails/cards'
+  get 'rails/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "cards#index"
 end
