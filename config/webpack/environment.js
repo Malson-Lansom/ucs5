09fd8ce6068f23c60a89d26e67bb90aa27705616
Malson-Lansom <61781906+Malson-Lansom@users.noreply.github.com>
2020-07-20 12:54:24 +0900

    bootstrap を有効にするためにPopper追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,10 +2,12 @@ const { environment } = require('@rails/webpacker')
 
 // jquery導入用
 const webpack = require('webpack')
+
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
         jQuery: 'jquery/src/jquery'
+        Popper: ['popper.js', 'default']
     })
 )
 
